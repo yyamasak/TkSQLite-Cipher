31341b6f1b0c495526afd0e2c01bf5793e23319d
Yusuke Yamasaki <yusuke.yamasaki@iasinc.jp>
2017-03-31 21:02:00 +0900

    2017-03-31  Yusuke Yamasaki  <yusuke.yamasaki@iasinc.jp>
    	* 0.9.4:
    	- new: sqlite3 v3.18.0 with wxSQLite3 v3.5.3 AES 128 bit encryption.

--- sqlitefunc/unixepoch.tcl
+++ sqlitefunc/unixepoch.tcl
@@ -1,5 +1,9 @@
 proc UnixEpoch {t} {
-	clock format $t -format "%Y-%m-%d %H:%M:%S"
+	set ret $t
+	if {[string is integer -strict $t]} {
+		set ret [clock format $t -format "%Y-%m-%d %H:%M:%S"]
+	}
+	return $ret
 }
 
 db function unixepoch UnixEpoch
