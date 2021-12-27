5ee44e1b36652f8ef273d394405d0a9e357c952f
Yusuke Yamasaki <yusuke.yamasaki@iasinc.jp>
2021-12-27 16:22:31 +0900

    2021-12-27  Yusuke Yamasaki  <yusuke.yamasaki@iasinc.jp>
    	* 0.9.23.1:
    	- new: sqlite3 v3.37.0 with SQLite3MultipleCiphers v1.3.5 AES 128 bit encryption by default and URI file names are supported.
    	  CFLAGS="-DCODEC_TYPE=CODEC_TYPE_AES128 -DSQLITE_USE_URI -msse4.2 -maes"

--- tksqlite.tcl
+++ tksqlite.tcl
@@ -117,7 +117,7 @@ ohtsuka.yoshio@gmail.com
 # - On Linux all tk widgets disallow keyboard input, if encoding system 
 #   is unicode.
 #;#>>>
-set VERSION 0.9.23
+set VERSION 0.9.24
 package require Tk 8.4
 package require Tktable
 if {[info tclversion] < 8.5} {
